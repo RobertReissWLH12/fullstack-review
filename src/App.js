d6e559153994bb0511d7d37f371c8924b5076556
Robert Reiss <reissaudiodesign@yahoo.com>
2019-11-14 15:49:27 -0700

    login works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
-export default class Add extends Component {
-  state = {
-    title: '',
-    imgUrl: '',
-    content: ''
-  }
-  render() {
-    return (
-      <div>
-        Add.js
-      </div>
-    )
-  }
+import React from 'react';
+import './App.css';
+import routes from './routes'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {routes}
+    </div>
+  );
 }
+
+export default App;
